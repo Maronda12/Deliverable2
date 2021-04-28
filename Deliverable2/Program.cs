46f598a7ab0bd6e2df76d168fa5a638ad0d7541d
Maronda Anderson-Johnson <66698985+Maronda12@users.noreply.github.com>
2021-04-28 13:45:16 -0400

    Add files via upload

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Deliverable2
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+
+
+         Console.WriteLine("Hello");
+
+            double d1 = 1;
+            double d2 = 1;
+            double d3 = 1;
+
+
+            while (true)
+            {
+                Console.WriteLine("What would you like to say to the bot?");
+                string userInput = Console.ReadLine().ToLower();
+
+                if (userInput == "hello" && d1 == 1)
+                {
+                    Console.WriteLine("Hi good to see you");
+                    d1++;
+                }
+                else if (userInput == "hello there" && d2 == 1)
+                {
+                    Console.WriteLine("General Kenobi");
+                    d2++;
+                }
+                else if (userInput == "sup" && d3 == 1)
+                {
+                    Console.WriteLine("I am good");
+                    d3++;
+                }
+                else if (userInput == "bye")
+                {
+                    Console.WriteLine("Thank you for using goodbye");
+                    return;
+                }
+                else if (d1 > 1)
+                {
+                    Console.WriteLine("I'm sorry but you have already said that");
+                }
+                else if (d2 > 1)
+                {
+                    Console.WriteLine("I'm sorry but you have already said that");
+                }
+                else if (d3 > 1)
+                {
+                    Console.WriteLine("I'm sorry but you have already said that");
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
